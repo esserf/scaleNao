8a223b70a308910427cec7ecb3de0ecc08256eec
Sigurd Sippel <sigurd.sippel@haw-hamburg.de>
2012-11-09 16:46:55 +0100

    fix

--- src/scaleNao/raw/messages/Message.scala
+++
@@ -1,8 +0,0 @@
-package scaleNao.raw.messages
-
-trait Message
-trait InfoMessage extends Message
-trait ErrorMessage extends Message
-
-case object OutMessageNotAllowed extends ErrorMessage
-case object InMessageNotAllowed extends ErrorMessage
